5309580b7ecab023db6763ac67a02e17d32d48b4
Piotr <latidude99@gmail.com>
2019-04-22 19:12:11 +0100

    Add copyright info to all classes

--- src/main/java/com/latidude99/sncxmlreader/controller/SearchPaneController.java
+++ src/main/java/com/latidude99/sncxmlreader/controller/SearchPaneController.java
@@ -1,32 +1,33 @@
 /**Copyright (C) 2017  Piotr Czapik.
-* @author Piotr Czapik
-* @version 2.0
-* 
-*  This file is part of SncXmlReader.
-*  Subs Converter is free software: you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation, either version 3 of the License, or
-*  (at your option) any later version.
-* 
-*  SncXmlReader is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-* 
-*  You should have received a copy of the GNU General Public License
-*  along with SncXmlReader.  If not, see <http://www.gnu.org/licenses/>
-*  or write to: latidude99@gmail.com
-*/
+ * @author Piotr Czapik
+ * @version 2.3
+ *
+ *  This file is part of SncXmlReaderFXDB.
+ *  SncXmlReaderFXDB is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SncXmlReaderFXDB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with SncXmlReaderFXDB.  If not, see <http://www.gnu.org/licenses/>
+ *  or write to: latidude99@gmail.com
+ */
 
 package com.latidude99.sncxmlreader.controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
 public class SearchPaneController implements Initializable{
--- src/test/java/FileCleanupTaskTests_tempDir.java
+++ src/test/java/FileCleanupTaskTests_tempDir.java
@@ -1,3 +1,23 @@
+/**Copyright (C) 2017  Piotr Czapik.
+ * @author Piotr Czapik
+ * @version 2.3
+ *
+ *  This file is part of SncXmlReaderFXDB.
+ *  SncXmlReaderFXDB is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SncXmlReaderFXDB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with SncXmlReaderFXDB.  If not, see <http://www.gnu.org/licenses/>
+ *  or write to: latidude99@gmail.com
+ */
+
 import com.latidude99.sncxmlreader.utils.FileCleanupTask;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.io.TempDir;
